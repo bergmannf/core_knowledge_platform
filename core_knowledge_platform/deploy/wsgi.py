6f7d4c3f0707b0b971fc8ec3840af4dc714af088
Florian Bergmann <Bergmann.F@gmail.com>
2011-08-15 17:19:12 +0100

    Added Gondor Files

---
+++ core_knowledge_platform/deploy/wsgi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import os, sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
+
+
+from django.core.handlers.wsgi import WSGIHandler
+os.environ["DJANGO_SETTINGS_MODULE"] = "core_knowledge_platform.settings"
+application = WSGIHandler()
